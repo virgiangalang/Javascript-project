a68430ff0dea83c068f229a48beceaf2bb4aa74e
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-04 23:29:41 +0700

    Menambahkan variabel 1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
+
+var thisIsFunction = "this is function";
+const PI = 3.14;
+
+var y = 10;
+var z = 5;
+
+// function dibawah ini akan mereturn angka dari hasil penambahan
+var x = y + z;
+function sumNum(a, b) {
+  return a + b;
+}
