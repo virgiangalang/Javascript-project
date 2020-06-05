430fc27aa0f06120e707a1aeb407be068be7613d
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-05 23:13:06 +0700

    Final 1

--- src/app.js
+++ src/app.js
@@ -1,20 +1,3 @@
-// //var dan let
-// function foo() {
-//   let fooVar = "bar";
-//   console.log("didalam func " + fooVar);
-//   return fooVar;
-// }
-// foo();
-
-// for (const i = 0; i < 3; i++) {
-//   console.log("hello " + i);
-// }
-// console.log(i);
-
-const nama = "Budi Subsidi";
-nama = "Gunawan Setiawan";
-console.log(nama);
-
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
