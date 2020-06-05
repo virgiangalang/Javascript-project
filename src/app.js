45b3290be6d8d9f17c383734adf3b7c19a39f3ff
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-05 22:47:51 +0700

    Belajar Var vs Let

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+//var dan let
+function foo() {
+  var fooVar = "bar";
+  console.log("didalam func " + fooVar);
+  return fooVar;
+}
+foo();
+
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
-
-var numberA = 100;
-var numberB = numberA + 50;
-
-console.log(numberB);
-// function dibawah ini akan mereturn angka dari hasil penambahan
