d36c3f3f3219a66d10042ae9ee031fecb681763d
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-08 21:50:11 +0700

    Exercise2

--- src/app.js
+++ src/app.js
@@ -2,3 +2,10 @@ const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
 //asdasd
+/*
+ *asfasf
+ *asdasdada
+ asdads
+ safasfa
+
+ */
