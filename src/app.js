6550bfe00abde61289c6e097ea0821671ecc35b0
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-10 20:55:31 +0700

    Arithmatic in JS

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
-//let numberdua = 2020;
+let additional = 100 + 200;
+let substraction = 200 - 100;
+let multiplication = 100 * 2;
+let division = 200 / 2;
+let edgeCase = additional - substraction * division;
+let modulo = 200 % 3;
+console.log();
 
 const projectTitle = document.querySelector("#project-title");
 
