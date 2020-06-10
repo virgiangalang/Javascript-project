736f4d7069f542d7abe2111e2d32e4d403a4b4fa
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-10 21:55:18 +0700

    String1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-let specialNumber1 = Infinity;
-let specialNumber2 = -Infinity;
-let specialNumber3 = NaN;
-let specialCase = Infinity - Infinity;
-console.log(specialCase);
+let multiLineString = "Ini adalah paragraf 1 dan \n Dan ini adalah paragraf 2";
+let multiLineString2 = `Ini adalah paragraf 1 
+Dan ini adalah paragraf 2`;
+
+let firstName = "David";
+let lastName = "Becham";
+let shirtNumber = 7;
 
 const projectTitle = document.querySelector("#project-title");
 
