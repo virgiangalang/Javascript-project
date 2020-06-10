22d57f535b0fe894f5ca96d8fa495b32931899fe
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-10 23:50:38 +0700

    start1

--- src/app.js
+++ src/app.js
@@ -1,14 +1,3 @@
-let multiLineString = "Ini adalah paragraf 1 dan \n Dan ini adalah paragraf 2";
-let multiLineString2 = `Ini adalah paragraf 1 
-Dan ini adalah paragraf 2`;
-
-let firstName = "David";
-let lastName = "Becham";
-let shirtNumber = 7;
-
-let namaLengkap = `${firstName} ${lastName} ${shirtNumber}`;
-console.log(namaLengkap);
-
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
