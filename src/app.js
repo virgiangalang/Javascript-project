028c055911a2cbb006130159c7cd6c03b564742f
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-11 19:41:34 +0700

    LogicAL OPERATOR

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 let tahun = 2020;
 console.log(tahun === 2019 + 1);
 
+let yes = !false;
+console.log(yes);
+
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
