2cea28fea6c10e14e7279233231684790043cef0
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-11 23:47:40 +0700

    make alert

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
-// let biodataObject = {
-//   firstName: "Bambang",
-//   lastName: "Sutarman",
-//   makananFavorit: ["Sate babi, Rica babi, Babi kecap"],
-//   istri: {
-//     firstName: "Siti Nurjeha",
-//     lastName: "Semloha",
-//     makananFavorit: ["Sate babi, Rica babi, Babi kecap"],
-//     anak: {
-//       firstName: "Budi Subsidi",
-//       lastName: "Abadi"
-//     }
-//   }
-// };
+alert("belajarlah, kamu GOBLOK!!!");
 
 const projectTitle = document.querySelector("#project-title");
 
