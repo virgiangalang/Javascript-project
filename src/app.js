8f6fbc0c1dd59c5ef0a9402b3629bc9741677302
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-12 22:18:28 +0700

    while

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+let count = 0;
+while (count < 20) {
+  console.log(count);
+  count = count + 2;
+}
+
+// do {
+//   console.log(count);
+//   count = count+2;
+// } while (count < 2);
+
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
