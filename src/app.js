87d3fc2336ea6903428c7d2a4473d2458ac8d70b
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-12 20:10:43 +0700

    Conditional execution 2

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 let yearOfBirth = Number(prompt("Kapan Tahun Lahirmu?"));
 
-if (2020 - yearOfBirth < 17 && 2020 - yearOfBirth > 10) {
+if (2020 - yearOfBirth < 10) {
   alert("Kamu masih anak anak");
+} else if (2020 - yearOfBirth > 10 && 2020 - yearOfBirth < 17) {
+  alert("Kamu masih Remaja");
+} else if (2020 - yearOfBirth > 18 && 2020 - yearOfBirth < 60) {
+  alert("Kamu Sudah Dewasa");
+} else if (2020 - yearOfBirth > 60 && 2020 - yearOfBirth < 80) {
+  alert("Kamu Sudah Dewasa");
 } else {
-  alert("Kamu sudah cukup Umur");
+  alert("GOBLOK KAMU COK!");
 }
 
 const projectTitle = document.querySelector("#project-title");
