3deb915fbd14fb004b55de4b0c737cf221948822
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-12 22:18:45 +0700

    do while

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-let count = 0;
-while (count < 20) {
+// let count = 0;
+// while (count < 20) {
+//   console.log(count);
+//   count = count + 2;
+// }
+
+do {
   console.log(count);
   count = count + 2;
-}
-
-// do {
-//   console.log(count);
-//   count = count+2;
-// } while (count < 2);
+} while (count < 2);
 
 const projectTitle = document.querySelector("#project-title");
 
