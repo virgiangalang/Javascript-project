78d99cab7202fe8b1f0749172189ba2bb8d3c902
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-12 21:10:28 +0700

    tenary operator

--- src/app.js
+++ src/app.js
@@ -2,15 +2,11 @@ let yearOfBirth = Number(prompt("Kapan Tahun Lahirmu?"));
 
 if (2020 - yearOfBirth < 10) {
   alert("Kamu masih anak anak");
-} else if (2020 - yearOfBirth > 10 && 2020 - yearOfBirth < 17) {
-  alert("Kamu masih Remaja");
-} else if (2020 - yearOfBirth > 18 && 2020 - yearOfBirth < 60) {
-  alert("Kamu Sudah Dewasa");
-} else if (2020 - yearOfBirth > 60 && 2020 - yearOfBirth < 80) {
-  alert("Kamu Sudah Dewasa");
 } else {
   alert("GOBLOK KAMU COK!");
 }
+
+2020 - yearOfBirth < 17 ? alert("Kamu masih kecil") : alert("kamu sudah tua");
 
 const projectTitle = document.querySelector("#project-title");
 
