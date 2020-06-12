7eb44a3ccf3b9c2f7a67cbff2b51e4f5892e2df4
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-12 21:57:51 +0700

    case conditional

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-let masalah = prompt("What's Your Problem?");
-switch (masalah) {
-  case "nothing":
-    alert("Good, remember you are in strong hands");
-    break;
-  default:
-    alert(
-      "Remember, God is bigger than your problem,just do your best then he will do His part"
-    );
-    break;
-}
-
 const projectTitle = document.querySelector("#project-title");
 
 projectTitle.textContent = "Title Baru dari JS File";
