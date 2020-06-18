4d9de55295539eb0d67529686944622d6a99b3d7
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-18 22:58:53 +0700

    Function change title

--- src/app.js
+++ src/app.js
@@ -1,17 +1,6 @@
-// const perpangkatan = function(n) {
-//   if (n == undefined) {
-//     return 5 * 5;
-//   }
-
-//   return n * n;
-// };
-
-const perpangkatan1 = (n = 10) => {
-  return n * n;
+const changeProjectHeadingTitle = title => {
+  const projectTitle = document.querySelector("#project-title");
+  projectTitle.textContent = title;
 };
 
-console.log(perpangkatan1(20));
-
-const projectTitle = document.querySelector("#project-title");
-
-projectTitle.textContent = "Title Baru dari JS File";
+changeProjectHeadingTitle("Title Baru dari JS File");
