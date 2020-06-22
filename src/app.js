39999297eae7943306a7e7881304e4e158f484ab
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-22 23:30:27 +0700

    array1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+//array
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const changeProjectHeadingTitle = title => {
   const projectTitle = document.querySelector("#project-title");
 
@@ -8,4 +11,4 @@ const changeProjectHeadingTitle = title => {
   }
 };
 
-changeProjectHeadingTitle(2020);
+changeProjectHeadingTitle("Javascript is EZ");
