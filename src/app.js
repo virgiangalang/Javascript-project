0f5b3f62d9a3828b4cbf1ba5ae6f083af3d36dd4
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-22 23:32:34 +0700

    array4

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 //array
-const array = [1, 2, 3, 4, 5, 6, 7, 8];
+const array = [1, 2, 3, 4, 5, 6, 7];
 
 const changeProjectHeadingTitle = title => {
   const projectTitle = document.querySelector("#project-title");
