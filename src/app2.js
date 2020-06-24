712f84cc8ed9a8bfac92b73b4a7ebae53ff191d0
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-24 20:50:59 +0700

    array push and pop

--- src/app2.js
+++ src/app2.js
@@ -1,7 +1,8 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
-
-// for (let i = 0; i < array.length; i++) {
-//   array[i] = array[i] * 5;
-// }
-console.log((array[0] = "satu"));
 console.log(array);
+
+//push method = menambah variabel baru
+array.push(9);
+
+//pop = menghilangkan variabel dibelakang
+array.pop();
