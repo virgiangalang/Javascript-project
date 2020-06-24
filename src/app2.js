d8b397b24864c4f7f9b2bf1f2a9f5a440f118900
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-24 20:57:52 +0700

    shift array

--- src/app2.js
+++ src/app2.js
@@ -1,8 +1,8 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log(array);
 //unshift = menambahkan value didepan array
-array.unshift(0);
+//array.unshift(0);
 
 //shift = menghilangkan value didepan
 array.shift();
+console.log(array);
