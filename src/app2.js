eda1a6cb5463908ac390d0da6ced2bc21f5c33df
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-24 20:42:53 +0700

    re asign variabel array

--- src/app2.js
+++ src/app2.js
@@ -1,6 +1,7 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
 
-for (let i = 0; i < array.length; i++) {
-  array[i] = array[i] * 5;
-}
+// for (let i = 0; i < array.length; i++) {
+//   array[i] = array[i] * 5;
+// }
+console.log((array[0] = "satu"));
 console.log(array);
