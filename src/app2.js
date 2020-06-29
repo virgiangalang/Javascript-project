a8e55ccbdf361f565ac0b403194606016f7c837e
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-29 21:37:21 +0700

    foreach n map

--- src/app2.js
+++ src/app2.js
@@ -7,3 +7,11 @@ animals.forEach(myFunction);
 function myFunction(value) {
   console.log(value + " hidup");
 }
+
+const numbers = [10, 20, 30, 40, 50, 60, 70];
+const multipliedNumber = numbers.map(multiply);
+function multiply(value) {
+  return value * 2;
+}
+
+console.log(multipliedNumber);
