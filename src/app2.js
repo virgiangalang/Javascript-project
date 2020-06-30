e19a3f09d7cd071518f0784cf15bcece20b28a73
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-30 23:00:51 +0700

    object assign2

--- src/app2.js
+++ src/app2.js
@@ -4,5 +4,5 @@
 const objects1 = { a: 1, b: 2, c: 3 };
 const objects2 = { c: 5, d: 10, e: 15 };
 
-Object.assign(objects2, objects1);
-console.log(objects2);
+Object.assign(objects1, objects2);
+console.log(objects1);
