4349baf88071c513aa60ec5e8de9995353c4d885
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-30 20:43:01 +0700

    delete 1

--- src/app2.js
+++ src/app2.js
@@ -7,14 +7,6 @@ const person = {
   age: 20,
   nationality: "indonesia"
 };
-
-const personName = person.name.firstName;
-console.log(personName);
-
-// console.log(person);
-
-// const person2 = new Object();
-// person2.firstName = "Virgian";
-// person2.lastName = "Galang";
-
-// console.log(person2);
+delete person.nationality;
+console.log(person);
+console.log(nationality);
