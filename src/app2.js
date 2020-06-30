0b2effb2e70798a95a28d891380b823b2e42d862
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-06-30 22:52:10 +0700

    object keys

--- src/app2.js
+++ src/app2.js
@@ -1,11 +1,4 @@
 //object
-const person = {
-  name: {
-    firstName: "Virgian",
-    lastName: "Galang"
-  },
-  age: 20,
-  nationality: "indonesia"
-};
+const objects = { a: 1, b: 2, c: 3 };
 
-console.log(person);
+console.log(objects);
